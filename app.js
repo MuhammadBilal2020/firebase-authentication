3af141a47b33c2a9af053a761940c756220d716f
BilalCh0ta <smit.11.aandw@gmail.com>
2024-07-26 01:12:16 +0500

    add files

---
+++ app.js
@@ -0,0 +1,26 @@
+import {  signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js"
+import { auth } from "./config.js"
+
+const form = document.querySelector('#forms')
+const email = document.querySelector('#email')
+const password = document.querySelector('#pass')
+
+
+form.addEventListener('submit' , (event)=>{
+event.preventDefault()
+
+
+signInWithEmailAndPassword(auth, email.value, password.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    window.location = "home.html"
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage);
+  });
+
+})
---
+++ config.js
@@ -0,0 +1,26 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+ 
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAYFOVk2R0OT2Yr1Jh4iyvFNxcXJuKbugA",
+    authDomain: "authentication-96633.firebaseapp.com",
+    projectId: "authentication-96633",
+    storageBucket: "authentication-96633.appspot.com",
+    messagingSenderId: "656896628812",
+    appId: "1:656896628812:web:07b1869d6f89aa463c7ce9",
+    measurementId: "G-8LH3Q6WKQX"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app)
+
+  
